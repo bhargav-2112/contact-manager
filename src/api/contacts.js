a6b2734d46dc5bed757ec4f54047c70bcc14b2a2
bhargav <bhargav.j@crestinfosystems.net>
2022-06-13 17:00:52 +0530

    contacts.js

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -1,9 +1,5 @@
 import axios from 'axios';
 
-// export default axios.create({
-//     baseURL: 'https://localhost:3006/',
-// });
-
 export default axios.create({
     baseURL: 'http://localhost:3006/',
     headers: {
